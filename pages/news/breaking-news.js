455f8179656711fbc522288e2d88a241de747b38
rawwr <akshayace0302@gmail.com>
2022-07-09 04:03:07 +0530

    nested routes

---
+++ pages/news/breaking-news.js
@@ -0,0 +1,7 @@
+//our-domain.com/news/breaking-news
+
+function BreakingNews() {
+  return <h1>The Breaking news page</h1>;
+}
+
+export default BreakingNews;
---
+++ pages/news/index.js
@@ -0,0 +1,7 @@
+//our-domain.com/news
+
+function NewsPage() {
+  return <h1>Here's goes the news: </h1>;
+}
+
+export default NewsPage;
